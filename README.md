39aea270533d9c23c109693ccd48280e574e9b3e
Bogdan <bogdandorca@gmail.com>
2015-04-28 23:20:18 +0300

    Angular set-up

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 ## API
 
-The API service can be accessed using /api/
+The API service can be accesses using the **/api/** route.
+
+## Autocomplete
+
+The autocomplete route takes a *String* formatted destination name as parameter.
+
+ > GET /api/autocomplete/:destination
--- server/repo/environment.js
+++ server/repo/environment.js
@@ -6,7 +6,13 @@ module.exports = function(app, express){
     app.set('view engine', 'jade');
     app.use(express.static('./public'));
 
-    app.get('*', function(req, res){
+    app.get('/', function(req, res){
         res.render('index');
+    });
+    app.get('/partials/*', function(req, res){
+        res.render('./partials/'+req.params[0]);
+    });
+    app.get('*', function(req, res){
+        res.end();
     });
 };
