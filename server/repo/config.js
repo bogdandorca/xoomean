b31f033c7e75ab6d278a66e422137a1d13c5d89f
Bogdan <bogdandorca@gmail.com>
2015-04-29 02:01:54 +0300

    Heroku temp fixes

--- server/repo/config.js
+++ server/repo/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     development: {
-        database: 'mongodb://localhost/xoomean',
+        //database: 'mongodb://localhost/xoomean',
+        database: 'mongodb://bogdandorca:instagram1@ds031632.mongolab.com:31632/xoomean',
         port: process.env.PORT || 3030
     },
     production: {
