d331a869f2ebe39fa92a49905908cbf516ecd0cd
Bogdan <bogdandorca@gmail.com>
2015-04-30 14:42:08 +0300

    Final bug-shooting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var env = process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var app = express();
 var config = require('./server/repo/config');
 // Environment general set-up
@@ -9,7 +9,7 @@ require('./server/repo/environment')(app, express);
 // Message styling helper
 var msg = require('./server/repo/message-log');
 // Database connection
-require('./server/repo/database')(config, msg);
+require('./server/repo/database')(config, msg, env);
 
 // Models
 require('./server/models/autocomplete')();
--- server/repo/database.js
+++ server/repo/database.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
-module.exports = function(config, msg){
-    mongoose.connect(config[process.env].database, function(err){
+module.exports = function(config, msg, env){
+    mongoose.connect(config[env].database, function(err){
         err ? console.log(msg.error(err)) : console.log(msg.success('Database server connection established.'));
     });
 };
