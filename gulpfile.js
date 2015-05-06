79a1740ecb4a91004b15cef8db0bfba49952f0f8
Bogdan Dorca <bogdan.dorca@xoomworks.com>
2015-05-06 17:02:57 +0300

    Gulp and Karma config

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon'),
+    sass = require('gulp-sass'),
+    minifyCss = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    ignore = require('gulp-ignore'),
+    rimraf = require('gulp-rimraf'),
+    merge = require('merge-stream'),
+    karma = require('gulp-karma');
+
+// Environment
+var styles = './public/styles';
+gulp.task('css-development', function(){
+    return gulp.src(styles+'/**/*.scss')
+        .pipe(sass())
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: true
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(minifyCss())
+        .pipe(concat('global.css'))
+        .pipe(gulp.dest(styles+'/'));
+});
+
+gulp.task('run', function(){
+    nodemon({
+        script: 'index.js',
+        ext: 'js jade',
+        env: { 'NODE_ENV': 'development' }
+    });
+    gulp.watch(styles+'/**/*.scss', ['css-development']);
+});
+gulp.task('test', function(){
+    karma.start({
+        configFile: 'karma.conf.js',
+        singleRun: true
+    });
+});
